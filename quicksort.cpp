cc9e26027e84a6f66fe06d765744bda51045c9f7
Sara <sara.rachmanczuk@gmail.com>
2026-03-16 22:43:46 +0100

    Add files via upload

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,10 +1,12 @@
-#include<iostream>
+#include <iostream>
 #include<vector>
 #include<ctime>
 #include<iomanip>
 #include<algorithm>
+#include<cmath>
+#include<cstdlib>
 using namespace std;
-
+//generacja typu danych
 vector<int> generujLosowe(int n) {
     vector<int> v(n);
     for(int i = 0; i < n; i++) v[i] = rand() % 100000;
@@ -53,48 +55,47 @@ string nazwaTypu(int typ) {
     if(typ == 5) return "V-ksztaltne";
     return "";
 }
-void shellsort(vector<int> &v,int n,int &liczbaporownan,int &liczbazamian,bool pokaz_przyrost=false)
+
+void quicksort(vector<int> & v, int left, int right,int &ilepor,int &ilezam,bool pokaz_pivot=false)
 {
-    if(v.size()<=1) return;
+    if(right<=left){return;}
+    int p = v[left];
+    if(pokaz_pivot) cout<<"wartosc pivota: "<<p<<endl;
 
-    int h = 1;
+    int i=left,j=right;
 
-    while(h < n/3)
-        h = 3*h + 1;
-
-    while(h >= 1)
+    while(i <= j)
     {
-        if(pokaz_przyrost) cout << "Aktualny przyrost h = " << h << endl;
-
-        for(int i = h; i < n; i++)
-        {
-            int temp = v[i];
-            int j = i;
-            
-            liczbaporownan++;
-            while(j >= h && v[j-h] < temp)
-            {
-                v[j] = v[j-h];
-                liczbazamian++;
-                j -= h;
-
-                if(j >= h) {
-                    liczbaporownan++;
-                }
-            }
-            
-            if(j != i) {
-                v[j] = temp;
-                liczbazamian++; 
-            }
+        ilepor++;
+        while(v[i] > p){
+            i++;
+            ilepor++;
         }
 
-        h = h/3;
+        ilepor++;
+        while(v[j] < p){
+            j--;
+            ilepor++;
+        }
+
+        if( i <= j){
+            swap(v[i], v[j]);
+            ilezam++;
+            i++;
+            j--;
+        }
     }
+
+    if(j > left){
+        quicksort(v, left, j,ilepor,ilezam,pokaz_pivot);
+    }
+    if(i < right){
+        quicksort(v, i, right,ilepor,ilezam,pokaz_pivot);
+    }
+
 }
 
-int main()
-{
+int main(){
     srand(time(0));
     int tryb;
     cout << "Wybierz tryb pracy programu:" << endl;
@@ -104,41 +105,39 @@ int main()
     cin >> tryb;
     if(tryb==1)
     {
-        int n,x;
-        vector<int> v;
+        vector<int> vec;
         cout<<"Podaj ilosc elementow: "<<endl;
+        int n,a;
         cin>>n;
-
         for(int i=0;i<n;i++)
         {
-            cin>>x;
-            v.push_back(x);
+            cin>>a;
+            vec.push_back(a);
         }
         cout<<"Ciag przed posortowaniem: "<<endl;
         for(int i=0;i<n;i++)
         {
-            cout<<v[i]<<" ";
+            cout<<vec[i]<<" ";
         }
         cout<<endl;
-        int liczbaporownan = 0,liczbazamian = 0;
-        bool pokaz_przyrost = true;
+        int ilepor = 0,ilezam = 0;
 
         clock_t start_time = clock();
-        shellsort(v,n,liczbaporownan,liczbazamian,pokaz_przyrost);
+        quicksort(vec, 0, vec.size()-1,ilepor,ilezam,true);
         clock_t end_time = clock();
         double seconds = (double)(end_time - start_time) / CLOCKS_PER_SEC;
         cout << endl<<"Czas sortowania: " << fixed << setprecision(6) << seconds << " sekundy" << endl;
         cout<<endl<<"Ciag po posortowaniu: "<<endl;
-        for(int i=0;i<n;i++)
-        {
-            cout<<v[i]<<" ";
+        for(int i = 0; i < vec.size(); i++){
+            cout << vec[i] << " ";
         }
 
-        cout<<endl<<"ile porownan: "<<liczbaporownan<<endl<<"ile zamian: "<<liczbazamian<<endl;
+        cout<<endl<<"ile porownan: "<<ilepor<<endl<<"ile zamian: "<<ilezam<<endl;
+        
     }
     else{
         int wzr_lin[] = {1000,2000,3000,4000,5000,6000,7000,8000,9000,10000};
-        int wzr_wykl[] = {1000,2000,4000,8000,16000,32000,64000,128000,256000,512000};
+        int wzr_wykl[] = {10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240, 20480};
 
         cout<<"Dla wzrostu liniowego: "<<endl;
         cout << "Rozmiar;Typ;Czas[s];Odchylenie" << endl;
@@ -151,18 +150,17 @@ int main()
                 
                 for(int j=0;j<10;j++)
                 {
-                    vector<int> v;
-                    if(typ==1){v = generujLosowe(wzr_lin[i]);}
-                    else if(typ==2){v = generujRosnace(wzr_lin[i]);}
-                    else if(typ==3){v = generujMalejace(wzr_lin[i]);}        
-                    else if(typ==4){v = generujA_ksztaltne(wzr_lin[i]);}
-                    else{v = generujV_ksztaltne(wzr_lin[i]);}
+                    vector<int> vec;
+                    if(typ==1){vec = generujLosowe(wzr_lin[i]);}
+                    else if(typ==2){vec = generujRosnace(wzr_lin[i]);}
+                    else if(typ==3){vec = generujMalejace(wzr_lin[i]);}        
+                    else if(typ==4){vec = generujA_ksztaltne(wzr_lin[i]);}
+                    else{vec = generujV_ksztaltne(wzr_lin[i]);}
 
                     int por = 0, zam = 0;
-                    bool pokaz_przyrost = false;
                     
                     clock_t start = clock();
-                    shellsort(v,v.size(),por,zam,pokaz_przyrost);
+                    quicksort(vec, 0,vec.size()-1, por, zam,false);
                     clock_t end = clock();
 
                     double sekundy = (double)(end - start) / CLOCKS_PER_SEC;
@@ -185,7 +183,7 @@ int main()
         cout<<"Dla wzrostu wykladniczego: "<<endl;
         cout << "Rozmiar;Typ;Czas[s];Odchylenie" << endl;
 
-        for(int i=0;i<10;i++)
+        for(int i=0;i<12;i++)
         {
             for(int typ = 1;typ<=5;typ++)
             {
@@ -194,18 +192,17 @@ int main()
                 
                 for(int j=0;j<10;j++)
                 {
-                    vector<int> v;
-                    if(typ==1){v = generujLosowe(wzr_wykl[i]);}
-                    else if(typ==2){v = generujRosnace(wzr_wykl[i]);}
-                    else if(typ==3){v = generujMalejace(wzr_wykl[i]);}        
-                    else if(typ==4){v = generujA_ksztaltne(wzr_wykl[i]);}
-                    else{v = generujV_ksztaltne(wzr_wykl[i]);}
+                    vector<int> vec;
+                    if(typ==1){vec = generujLosowe(wzr_wykl[i]);}
+                    else if(typ==2){vec = generujRosnace(wzr_wykl[i]);}
+                    else if(typ==3){vec = generujMalejace(wzr_wykl[i]);}        
+                    else if(typ==4){vec = generujA_ksztaltne(wzr_wykl[i]);}
+                    else{vec = generujV_ksztaltne(wzr_wykl[i]);}
 
                     int por = 0, zam = 0;
-                    bool pokaz_przyrost = false;
                     
                     clock_t start = clock();
-                    shellsort(v, v.size(),por,zam,pokaz_przyrost);
+                    quicksort(vec, 0,vec.size()-1, por, zam,false);
                     clock_t end = clock();
 
                     double sekundy = (double)(end - start) / CLOCKS_PER_SEC;
@@ -225,6 +222,5 @@ int main()
             }
         }
     }
-
     return 0;
 }
